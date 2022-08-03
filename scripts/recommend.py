f22cd3d8de854ed5d47a4e770d3035a4a7e740be
Derrick Adam <78511177+dadam1026@users.noreply.github.com>
2022-08-02 20:13:40 -0400

    Add files via upload

---
+++ scripts/recommend.py
@@ -0,0 +1,21 @@
+import pickle
+import pandas as pd
+
+df_cc = pd.read_pickle('models/cc_df.pkl')
+df_ccs = pd.read_pickle('models/ccs_df.pkl')
+df_chianti = pd.read_pickle('models/chi_df.pkl')
+df_cin = pd.read_pickle('models/cin_df.pkl')
+df_pv = pd.read_pickle('models/pv_df.pkl')
+
+
+def recommendations(input_string):
+    if "Pai" in input_string:
+        return df_pv.head(10)
+    elif "hurch" in input_string:
+        return  df_ccs.head(10)
+    elif "hianti" in input_string:
+        return df_chianti.head()
+    elif "innamon" in input_string:
+        return df_cin.head(10)
+    elif "ontainer" in input_string:
+        return df_cc.head(10)
